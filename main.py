c74ca5d76170a53b3e7077cfb704aef33144cbf1
dotrdp <rckydp1@gmail.com>
2025-07-14 11:11:13 -0600

    cleaned workdir

--- main.py
+++
@@ -1,6 +0,0 @@
-from API import StardewModdingAPI
-
-class Player():
-    def __init__(self, game_instance, environment=None):
-        self.game_instance = game_instance
-        self.environment = environment
