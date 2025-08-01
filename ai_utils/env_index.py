1ad8ecae2d440bd8ff3ae429473388292debc93a
dotrdp <rckydp1@gmail.com>
2025-08-01 00:39:08 -0600

    up for testing21

--- ai_utils/env_index.py
+++ ai_utils/env_index.py
@@ -2,23 +2,23 @@ import torch
 import networkx as nx
 
 items = {
-    "Pickaxe": 1,
-    "Axe": 2,
-    "MeeleWeapon": 3,
-    "nothing": 0,
-    "wood": 4,
-    "stone": 5,
+    "Pickaxe": 1.0,
+    "Axe": 2.0,
+    "MeeleWeapon": 3.0,
+    "nothing": 0.0,
+    "wood": 4.0,
+    "stone": 5.0,
 }
 # str -> index -> mlp
 
 seasons = {
-    "Spring": 1,
-    "Summer": 2,
-    "Autumn": 3,
-    "Winter": 4,
+    "Spring": 1.0,
+    "Summer": 2.0,
+    "Autumn": 3.0,
+    "Winter": 4.0,
 }
 
-def get_fixed_neighborhood_vector(energy_graph, player_node, nodes, radius=30, max_nodes=120):
+def get_fixed_neighborhood_vector(energy_graph, player_node, nodes,tile_dataset, radius=30, max_nodes=120):
     # Find nodes within a circle (graph distance) around player_node
     circle_nodes = [
         node for node in nodes
@@ -40,6 +40,15 @@ def get_fixed_neighborhood_vector(energy_graph, player_node, nodes, radius=30, m
                 length = nx.shortest_path_length(energy_graph, source=player_node, target=node)
                 # Normalize length to [-1, 1] based on radius
                 lengths.append(length)
+                x, y = node
+                prop = tile_dataset[x][y].properties
+                if "collision" in prop:
+                    if "tool" in prop:
+                        if prop["tool"] in items:
+                            lengths[-1] = items[prop["tool"]]
+                        else:
+                            lengths[-1] = -1.0
+                    lengths[-1] = -2.0
             except nx.NetworkXNoPath:
                 lengths.append(-1.0)
     return lengths
@@ -77,7 +86,7 @@ def get_state_embedding(env, player) -> torch.Tensor:
     energy_graph = env.get_energy_graph()
     nodes = energy_graph.nodes
     player_node = player.position
-    lengths = torch.tensor(get_fixed_neighborhood_vector(energy_graph, player_node, nodes))
+    lengths = torch.tensor(get_fixed_neighborhood_vector(energy_graph, player_node, nodes, env))
 
     # suppose we want a circle around the player node, so the amount of nodes feed into the model and the dimensionality of the resultant vector is always the same
     print(" WE ROLLIGN")
