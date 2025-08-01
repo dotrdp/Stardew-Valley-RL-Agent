035288de07f14c5df3175cb18f80b8323b17d814
dotrdp <rckydp1@gmail.com>
2025-07-31 23:18:54 -0600

    up for testing4

--- ai_utils/env_index.py
+++ ai_utils/env_index.py
@@ -44,9 +44,9 @@ def get_state_embedding(env, player) -> torch.Tensor:
 
     # Energy graph: aggregate normalized 'we' within normalized distance <= 1
     energy_graph = env.get_energy_graph()
-    nodes = list(energy_graph.nodes)
+    nodes = (energy_graph.nodes)
     x, y = player.position
-    player_node = nodes[f"({str(x)}, {str(y)})"] #type: ignore
+    player_node = nodes[player.position]
 
     # Find all shortest path lengths from player_node
     lengths = nx.single_source_shortest_path_length(energy_graph, player_node, cutoff=20)
